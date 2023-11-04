352de6d30dba9d76bd92b44834dc291facd62d1e
RinesaGrabovci8 <rinesagrabovci08@gmail.com>
2023-11-04 02:18:54 +0200

    .

--- edmaarchitecture/src/Components/Aboutus/Aboutus.jsx
+++ edmaarchitecture/src/Components/Aboutus/Aboutus.jsx
@@ -4,15 +4,20 @@ import house from '../../assets/house.jpg';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/opacity.css';
+import { Transition } from 'react-transition-group';
+import { useState, useEffect } from 'react';
 
 const Aboutus = () => {
+    const [showImage, setShowImage] = useState(false);
 
   return (
     <div className='aboutus'>
-      <Grid  container spacing={2}>
+      <Grid container spacing={2}>
         <Grid item xs={12} sm={6}>
-          <div className='about' style={{margin:"150px 100px"}}>
-            <Typography style={{color:"#D32F2F",  fontWeight: 'bold', marginBottom:'30px' }} variant='h4' component='h1'>
+          <div className='about' style={{ margin: "150px 100px" }}>
+            <Typography style={{ color: "#D32F2F", fontWeight: 'bold', marginBottom: '30px' }} variant='h4' component='h1'>
               - ABOUT US
             </Typography>
             <Typography style={{ fontWeight: 'bold' }} paragraph>
@@ -21,16 +26,34 @@ const Aboutus = () => {
             <Typography paragraph>
               Founded in 2010 by a team of engineering experts, it has completed more than 400 projects over a period of more than a decade with varying expertise in civil engineering and consulting. We strive to adapt our projects to the latest developments and to use cutting-edge technology, always focusing on the customer's wishes, which we consider the key to success. Our aim is to develop our projects with high quality, functionality, aesthetics, and usability by providing ideas and advice on what the space in which you live, work, or relax will look like.
             </Typography>
-            <button  className='more'>
-                MORE...
-            </button>
+            <Button className='more-button' variant="contained" color="primary">
+              MORE...
+            </Button>
           </div>
         </Grid>
         <Grid item xs={12} sm={6}>
           <div className='image'>
-            <img style={{height:"500px", weight:"500px",backgroundColor:'black', opacity:'300', marginTop:'90px'}} src={house} alt="house" />
-            <Typography style={{color:'white', marginTop:'-310px', marginLeft:'190px', fontSize:'100px'}} variant='h3' component='h1'>
+            {/* <img style={{ height: "500px", weight: "500px", backgroundColor: 'black', opacity: '300', marginTop: '90px' }} src={house} alt="house" /> */}
+            <LazyLoadImage
+              alt="house"
+              height={500}
+              width={500}
+              src={house}
+              effect="opacity"
+              afterLoad={() => setShowImage(true)}
+              wrapperProps={{
+                style: {
+                  backgroundColor: 'black',
+                  opacity: showImage ? 1 : 0,
+                  transition: 'opacity 1s ',
+                },
+              }}
+            />
+            <Typography style={{ color: 'white', marginTop: '-350px', marginLeft: '160px', fontSize: '160px', fontWeight:'bold' }} variant='h3' component='h1'>
               15
+            </Typography>
+            <Typography style={{ color: 'white', marginTop: '20px', marginLeft: '140px', fontSize: '20px', fontWeight:'bold' }} variant='h3' component='h1'>
+              YEARS OF EXPERIENCE
             </Typography>
           </div>
         </Grid>
