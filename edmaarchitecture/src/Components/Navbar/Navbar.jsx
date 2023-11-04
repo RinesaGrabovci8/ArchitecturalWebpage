5681097922a4387f6af0dae721ff86a3a7096da9
RinesaGrabovci8 <rinesagrabovci08@gmail.com>
2023-11-04 18:05:30 +0200

    .

--- edmaarchitecture/src/Components/Navbar/Navbar.jsx
+++ edmaarchitecture/src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import "./navbar.scss";
 import { NavLink } from "react-router-dom";
-import MenuIcon from "@mui/icons-material/Menu";
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
 import { Context } from "../../Context/Context";
 import LangFlag from "./LangFlag";
 import { language } from "../../lang";
@@ -9,51 +9,59 @@ import logo from '../../assets/logo.png';
 import Grid from "@mui/material/Grid";
 
 const Navbar = () => {
-  const {lang, dispatch} = useContext(Context);
+  const { lang, dispatch } = useContext(Context);
   const [menuVisible, setMenuVisible] = useState(false);
+  const [menuIconDisplay, setMenuIconDisplay] = useState('none'); 
 
   const toggleMenu = () => {
     setMenuVisible(!menuVisible);
   };
 
-  const changelang = () => {
-    const setlang = lang === "en" ? "al" : "en";
+  const changeLang = () => {
+    const setLang = lang === "en" ? "al" : "en";
     dispatch({
       type: "LANG",
-      payload: { lang: setlang },
+      payload: { lang: setLang },
     });
   };
+
+  window.addEventListener('resize', () => {
+    if (window.innerWidth < 769) {
+      setMenuIconDisplay('block');
+    } else {
+      setMenuIconDisplay('none');
+    }
+  });
 
   return (
     <div className="navbar-container">
       <div className="navbar-wrapper">
         <Grid container alignItems="center">
-          <Grid item xs={12} sm={6}>
+          <Grid item xs={8} sm={6}>
             <NavLink to="/" className="logo-link">
               <img src={logo} alt="logo" />
             </NavLink>
           </Grid>
-          <Grid item xs={12} sm={6} container justifyContent="flex-end">
-            {window.innerWidth <= 600 && (
-              <MenuIcon
-              className="menu-icon"
-              onClick={toggleMenu}
-              style={{ display: 'block', color: 'white', marginTop:'-40px', marginLeft:'50px', marginRight:'50px' }}/>
-            )}
-            <ul className={`nav-links ${menuVisible ? "active" : ""}`}>
-              {language[lang].navbarLinks.map((obj) => (
-                <li key={obj.name}>
-                  <NavLink to={obj.path} onClick={() => setMenuVisible(false)}>
-                    {obj.name}
-                  </NavLink>
-                </li>
-              ))}
-            </ul>
-            <button className="langbutton" onClick={changelang}>
-              <LangFlag lang={lang} />
+          <Grid item xs={4} sm={6} container justifyContent="flex-end">
+            <button className="menuicon" onClick={toggleMenu} style={{display: menuIconDisplay }}>
+              <MenuRoundedIcon />
             </button>
           </Grid>
         </Grid>
+        <ul className={`nav-links ${menuVisible ? "active" : ""}`}>
+          {language[lang].navbarLinks.map((obj) => (
+            <li key={obj.name}>
+              <NavLink to={obj.path} onClick={() => setMenuVisible(false)}>
+                {obj.name}
+              </NavLink>
+            </li>
+          ))}
+          <li className="langbutton">
+            <button onClick={changeLang}>
+              <LangFlag lang={lang} />
+            </button>
+          </li>
+        </ul>
       </div>
     </div>
   );
--- edmaarchitecture/src/Pages/HomePage/Home.jsx
+++ edmaarchitecture/src/Pages/HomePage/Home.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import './home.scss'
 import Slider from '../../Components/Slider/Slider'
-import Navbar from '../../Components/Navbar/Navbar'
+ import Navbar from '../../Components/Navbar/Navbar'
 import Footer from '../../Components/Footer/Footer'
 import Aboutus from '../../Components/Aboutus/Aboutus'
 import Specialization from '../../Components/Specialization/Specialization'
 import Clients from '../../Components/Clients/Clients'
+// import Header from '../../Components/Navbar/Header'
 const Home = () => {
   return (
     <div className='home'>
-        <Navbar/>
+        <Navbar/> 
         <Slider/>
         <Aboutus/>
         <Specialization/>
